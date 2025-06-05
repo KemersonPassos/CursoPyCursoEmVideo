11b4fa9ba4836296aad3147c33ba104e505f046d
Kemerson Passos <125939081+KemersonPassos@users.noreply.github.com>
2025-06-05 10:53:01 -0300

    inicaiando exercícios em python

---
+++ expython01.py
@@ -0,0 +1,14 @@
+nome = input ("Qual é o seu nome?")
+print ("Seja bem vindo, ", nome, "!")
+
+data = input ("Que dia você nasceu?")
+mes = input ("Que mês você nasceu?")
+ano = input ("Que ano você nasceu?")
+
+print (nome, ", você nasceu em", data, "de", mes, "de", ano)
+
+num = input ("Agora,digite um número:")
+num2 = input ("Agora,digite outro número:")
+soma = int(num) + int(num2)
+
+print ("A soma dos dois números é", soma)
