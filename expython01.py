9c68bba8b13a7f5fc0b5c82bd9212b777115fd4d
Kemerson Passos <125939081+KemersonPassos@users.noreply.github.com>
2025-06-05 11:08:28 -0300

    Update expython01.py

--- expython01.py
+++ expython01.py
@@ -7,8 +7,8 @@ ano = input ("Que ano você nasceu?")
 
 print (nome, ", você nasceu em", data, "de", mes, "de", ano)
 
-num = input ("Agora,digite um número:")
-num2 = input ("Agora,digite outro número:")
+num = input ("Agora, digite um número:")
+num2 = input ("Agora, digite outro número:")
 soma = int(num) + int(num2)
 
 print ("A soma dos dois números é", soma)
