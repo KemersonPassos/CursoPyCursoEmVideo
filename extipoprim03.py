b4b81318906a7d9634019fe141e30205a19689de
Kemerson Passos <125939081+KemersonPassos@users.noreply.github.com>
2025-08-15 01:15:49 -0300

    update ex03 e 01

--- extipoprim03.py
+++ extipoprim03.py
@@ -1,5 +1,17 @@
-n = bool(input("Digite algo: "))
+n = (input("Digite algo: "))
 print("O tipo primitivo desse valor é", type(n))
 
+print("Só tem espaços?", n.isspace())
+print("É um número?", n.isnumeric())
+print("É alfabético?", n.isalpha())
+print("É alfanumérico?", n.isalnum())
+print("Está em maiúsculas?", n.isupper())
+print("Está em minúsculas?", n.islower())
+print("Está capitalizada?", n.istitle())
+print("É um dígito?", n.isdigit())
+print("É um identificador?", n.isidentifier())
+print("É imprimível?", n.isprintable())
+print("É um email?", "@" in n and "." in n) 
 
-#exercicio de tipos primitivos e input
+
+#exercicio "dissecando variaveis" de tipos primitivos e input
